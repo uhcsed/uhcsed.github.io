61b2ff15cb9a109342e71eaff0dc663b297bec31
zhyeong <z.hyeong@kaist.ac.kr>
2024-11-11 15:01:04 +0900

    redirected every courses

--- next.config.js
+++ next.config.js
@@ -10,6 +10,100 @@ const nextConfig = {
         destination: 'https://cs374-spring-2023.netlify.app',
         permanent: true,
       },
+      {
+        source: '/courses/cs374-spring-2021',
+        destination: 'https://cs374-spring-2021.netlify.app',
+        permanent: true,
+      },
+      {
+        source: '/courses/cs374-spring-2020',
+        destination: 'https://cs374-spring-2020.netlify.app',
+        permanent: true,
+      },
+      {
+        source: '/courses/cs374-spring-2019',
+        destination: 'https://cs374-spring-2019.netlify.app',
+        permanent: true,
+      },
+      {
+        source: '/courses/cs374-spring-2018',
+        destination: 'https://cs374-spring-2018.netlify.app',
+        permanent: true,
+      },
+      {
+        source: '/courses/cs374-spring-2017',
+        destination: 'https://cs374-spring-2017.netlify.app',
+        permanent: true,
+      },
+
+      {
+        source: '/courses/cs473-fall-2023',
+        destination: 'https://cs473-fall-2023.netlify.app',
+        permanent: true,
+      },
+      {
+        source: '/courses/cs473-fall-2021',
+        destination: 'https://cs473-fall-2021.netlify.app',
+        permanent: true,
+      },
+      {
+        source: '/courses/cs473-fall-2020',
+        destination: 'https://cs473-fall-2020.netlify.app',
+        permanent: true,
+      },
+      {
+        source: '/courses/cs473-fall-2019',
+        destination: 'https://cs473-fall-2019.netlify.app',
+        permanent: true,
+      },
+      {
+        source: '/courses/cs473-fall-2018',
+        destination: 'https://cs473-fall-2018.netlify.app',
+        permanent: true,
+      },
+
+      {
+        source: '/courses/cs473-fall-2017',
+        destination: 'https://cs492-fall-2017.netlify.app/', //social computing was cs492 back then
+        permanent: true,
+      },
+      {
+        source: '/courses/cs473-fall-2016',
+        destination: 'https://cs492-fall-2016.netlify.app', //social computing was cs492 back then
+        permanent: true,
+      },
+
+      {
+        source: '/courses/cs492-fall-2023',
+        destination: 'https://cs492-fall-2023.netlify.app',
+        permanent: true,
+      },
+      {
+        source: '/courses/cs492-spring-2021',
+        destination: 'https://human-ai.kixlab.org/',
+        permanent: true,
+      },
+      {
+        source: '/courses/cs492-fall-2020',
+        destination: 'https://cs492-fall-2020.netlify.app',
+        permanent: true,
+      },
+
+      {
+        source: '/courses/i2r-spring-2020',
+        destination: 'https://cs492d-spring-2020.netlify.app/',
+        permanent: true,
+      },
+      {
+        source: '/courses/i2r-spring-2019',
+        destination: 'https://nmsl.kaist.ac.kr/courses/i2r/index.html',
+        permanent: true,
+      },
+      {
+        source: '/courses/i2r-fall-2018',
+        destination: 'https://i2r.netlify.app/',
+        permanent: true,
+      },
     ]
   },
 }
